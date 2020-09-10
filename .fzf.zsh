e53ec4b9bef266f0ad191948618edb77c3637c1d
asrar <asrar@yarikul.in>
2020-09-11 01:16:10 +0530

    add i3 config and make config portable

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/asrar/.fzf/bin* ]]; then
+if [[ ! "$PATH" == */$HOME/.fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/home/asrar/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/asrar/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/asrar/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
